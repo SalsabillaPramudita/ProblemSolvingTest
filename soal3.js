59e8c82a71bde70eb4df9697b5f51a8d520884db
SalsabillaPramudita <110925630+SalsabillaPramudita@users.noreply.github.com>
2023-07-18 10:33:50 +0700

    commit soal3.js

--- soal3.js
+++ soal3.js
@@ -1,29 +1,32 @@
+const readline = require('readline');
+
 function isBalanced(expression) {
-    const stack = [];
-    const openingBrackets = ['(', '[', '{'];
-    const closingBrackets = [')', ']', '}'];
-    const bracketPairs = {')': '(', ']': '[', '}': '{'};
-  
-    for (let char of expression) {
-      if (openingBrackets.includes(char)) {
-        stack.push(char);
-      } else if (closingBrackets.includes(char)) {
-        if (stack.length === 0 || stack[stack.length - 1] !== bracketPairs[char]) {
-          return "NO";
-        }
-        stack.pop();
+  const stack = [];
+  const openingBrackets = ['(', '[', '{'];
+  const closingBrackets = [')', ']', '}'];
+  const bracketPairs = {')': '(', ']': '[', '}': '{'};
+
+  for (let char of expression) {
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      if (!stack.length || stack[stack.length - 1] !== bracketPairs[char]) {
+        return "NO";
       }
+      stack.pop();
     }
-  
-    return stack.length === 0 ? "YES" : "NO";
   }
-  
-  // Menerima input dari pengguna
-  const expression = prompt("Masukkan urutan tanda kurung:");
-  
-  // Memanggil fungsi untuk memeriksa keseimbangan tanda kurung
+
+  return stack.length === 0 ? "YES" : "NO";
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Masukkan urutan tanda kurung: ', (expression) => {
   const result = isBalanced(expression);
-  
-  // Mencetak hasil
   console.log("Output:", result);
-  
+  rl.close();
+});
