878bb87f4e92e15203d09a2f62f4a476336baf7c
SalsabillaPramudita <110925630+SalsabillaPramudita@users.noreply.github.com>
2023-07-18 10:24:18 +0700

    commit soal1.js

--- soal1.js
+++ soal1.js
@@ -1,22 +1,31 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function generateSequence(n) {
-    const sequence = [1]; // Memulai deret dengan elemen pertama 1
-    
-    for (let i = 2; i <= n; i++) {
-      // Menghitung elemen berikutnya berdasarkan rumus A000124
-      const nextElement = sequence[i - 2] + i - 1;
-      sequence.push(nextElement);
-    }
-    
-    return sequence;
+  const sequence = [1]; // Memulai deret dengan elemen pertama 1
+
+  for (let i = 2; i <= n; i++) {
+    // Menghitung elemen berikutnya berdasarkan rumus A000124
+    const nextElement = sequence[i - 2] + i - 1;
+    sequence.push(nextElement);
   }
-  
-  // Menerima input dari pengguna
-  const n = parseInt(prompt("Masukkan jumlah elemen dalam deret:"));
-  
+
+  return sequence;
+}
+
+rl.question('Masukkan jumlah elemen dalam deret: ', (input) => {
+  const n = parseInt(input);
+
   // Memanggil fungsi untuk menghasilkan deret sesuai input
   const result = generateSequence(n);
-  
+
   // Mencetak deret hasilnya
   const output = result.join("-");
   console.log("Output:", output);
-  
+
+  rl.close();
+});
